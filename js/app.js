f581b348670b23beaad44b19a060d8d0fdbf973e
Nde-Code <nathan.debilloez@outlook.com>
2026-02-28 14:54:32 +0100

    feat: remove inline CSS from HTML pages.

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ $(window).on('resize', () => {
 function getNotesTemplate() {
 
     return `
-    <div class="modalbox-class" id="modalbox-notes" role="dialog" style="display:none;">
+    <div class="modalbox-class" id="modalbox-notes" role="dialog">
 
         <header class="modalbox-header-class">
 
@@ -43,17 +43,17 @@ function getNotesTemplate() {
 
                 <p class="modalbox-description"> J'utilise des bibliothèques soigneusement choisies, privilégiant celles qui sont personnalisables, légères et sécurisées, dans le but d'optimiser votre expérience utilisateur et de garantir un confort maximal: </p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Font Awesome:</strong> pour les petits icons un peu partout sur le site. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Font Awesome:</strong> pour les petits icons un peu partout sur le site. </p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>JQuery:</strong> pour une gestion plus éfficace du DOM en Javascript. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>JQuery:</strong> pour une gestion plus éfficace du DOM en Javascript. </p>
                    
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Math.js:</strong> pour les outils mathématiques utilisés dans mes projets. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Math.js:</strong> pour les outils mathématiques utilisés dans mes projets. </p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Plotly.js:</strong> pour dessiner des graphiques dans le navigateur. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Plotly.js:</strong> pour dessiner des graphiques dans le navigateur. </p>
 
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Ace.js:</strong> un petit éditeur de code écrit en JavaScript, accompagné des compléments nécessaires, notamment un ou plusieurs thèmes ainsi qu'une ou plusieurs colorations syntaxiques. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Ace.js:</strong> un petit éditeur de code écrit en JavaScript, accompagné des compléments nécessaires, notamment un ou plusieurs thèmes ainsi qu'une ou plusieurs colorations syntaxiques. </p>
                 
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Leaflet.js:</strong> pour créer des cartes interactives en JavaScript. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Leaflet.js:</strong> pour créer des cartes interactives en JavaScript. </p>
 
             </div>
 
@@ -65,17 +65,17 @@ function getNotesTemplate() {
 
                 <p class="modalbox-description"> Pour créer ce site, je ne me suis pas contenté d'un simple bloc-notes et de quelques images trouvées sur Internet (d'autant plus que je porte une attention particulière aux droits d'auteur). C'est pourquoi j'ai utilisé différents outils et ressources pour le concevoir: </p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Visual Studio Code:</strong> pour rédiger et modifier le code de ce chaleureux site internet. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Visual Studio Code:</strong> pour rédiger et modifier le code de ce chaleureux site internet. </p>
                                         
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Pixlr:</strong> pour retoucher l'image de fond du site.</p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Pixlr:</strong> pour retoucher l'image de fond du site.</p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Logomakr:</strong> pour créer mon "favicon".</p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Logomakr:</strong> pour créer mon "favicon".</p>
                    
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Perchance:</strong> pour réaliser l'avatar sur la page d'accueil.</p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Perchance:</strong> pour réaliser l'avatar sur la page d'accueil.</p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Flaticon:</strong> pour les différentes ressources utilisés dans mes projets. je renseigne toujours l'utilisation de ces graphismes dans les pieds de page ou dans un fichier README.Md. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Flaticon:</strong> pour les différentes ressources utilisés dans mes projets. je renseigne toujours l'utilisation de ces graphismes dans les pieds de page ou dans un fichier README.Md. </p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Pixabay:</strong> pour l'image de fond. Le lien est dans le "README.md" disponible sur le repos GitHub du projet. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Pixabay:</strong> pour l'image de fond. Le lien est dans le "README.md" disponible sur le repos GitHub du projet. </p>
                   
             </div>
 
@@ -85,9 +85,9 @@ function getNotesTemplate() {
 
                 <h2>Services tiers:</h2>
 
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>Goatcounter:</strong> pour obtenir certaines informations telles que le nombre de visiteurs, leur localisation (pays uniquement), leur système d'exploitation, ... Ce service est entièrement conforme au RGPD et ne nécessite pas de bannière de cookies. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>Goatcounter:</strong> pour obtenir certaines informations telles que le nombre de visiteurs, leur localisation (pays uniquement), leur système d'exploitation, ... Ce service est entièrement conforme au RGPD et ne nécessite pas de bannière de cookies. </p>
 
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>MapTiler:</strong> pour réaliser des cartes avec Leaflet.js, j'utilise un service qui fournit des tuiles raster un peu plus qualitatives que celles d'OpenStreetMap. </p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>MapTiler:</strong> pour réaliser des cartes avec Leaflet.js, j'utilise un service qui fournit des tuiles raster un peu plus qualitatives que celles d'OpenStreetMap. </p>
 
             </div>
 
@@ -97,9 +97,9 @@ function getNotesTemplate() {
 
                 <h2>Licence:</h2>
 
-                <p class="modalbox-description">Bien que je ne voie pas trop l'intérêt de reprendre l'intégralité du code de mon site pour le réutiliser tel quel, j'ai tout de même décidé de le placer sous <span style="color: #28A745; font-weight: bold;">une licence permissive</span> mais <span style="color: #DC3545; font-weight: bold;">pas trop non plus</span> (ça reste à la base un site personnel, entièrement codé à la main, sans aucun outil). Si vous souhaitez simplement <span style="color: #28A745; font-weight: bold;">reprendre quelques morceaux de code ou des éléments de style</span>, pas de souci. En revanche, pour <span style="color: #DC3545; font-weight: bold;">une réutilisation plus large ou complète</span>, merci de me contacter au préalable pour qu'on en discute. Voici la licence: </p>
+                <p class="modalbox-description">Bien que je ne voie pas trop l'intérêt de reprendre l'intégralité du code de mon site pour le réutiliser tel quel, j'ai tout de même décidé de le placer sous <span class="color-green">une licence permissive</span> mais <span class="color-red">pas trop non plus</span> (ça reste à la base un site personnel, entièrement codé à la main, sans aucun outil). Si vous souhaitez simplement <span class="color-green">reprendre quelques morceaux de code ou des éléments de style</span>, pas de souci. En revanche, pour <span class="color-red">une réutilisation plus large ou complète</span>, merci de me contacter au préalable pour qu'on en discute. Voici la licence: </p>
                     
-                <p class="modalbox-description"><i class="fa-solid fa-square" style="color: #4690F2; font-size: 14px;"></i> <strong>GNU General Public License v3.0:</strong> consultez le dépôt GitHub du projet pour en savoir plus sur cette licence.</p>
+                <p class="modalbox-description"><i class="fa-solid fa-square point"></i> <strong>GNU General Public License v3.0:</strong> consultez le dépôt GitHub du projet pour en savoir plus sur cette licence.</p>
                 
             </div>
 
